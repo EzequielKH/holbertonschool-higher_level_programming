4cc6b48fa3b6bc1d9f901240fd522fb7819dbbdc
EzequielKH <laudarosa12@gmail.com>
2025-09-17 13:18:49 +0000

    pyco_0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
-        new_row = [x**2 for x in row]  
+        new_row = [x**2 for x in row]
         new_matrix.append(new_row)
     return new_matrix
-    
