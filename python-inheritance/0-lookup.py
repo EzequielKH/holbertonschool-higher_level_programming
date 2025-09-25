377624b9336c2e14b3ae10b447a3b041bb720f1c
EzequielKH <laudarosa12@gmail.com>
2025-09-25 18:04:05 +0000

    made-0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+def lookup(obj):
+
+    """
+    Returns a list of available attributes and methods of an object,
+    excluding common dunder methods.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        A list of strings, where each string is the name of an attribute or method.
+    """
+    all_members = dir(obj)
+    filtered_members = [
+        member for member in all_members 
+        if not (member.startswith('__') and member.endswith('__') and len(member) > 4)
+    ]
+    return filtered_members
