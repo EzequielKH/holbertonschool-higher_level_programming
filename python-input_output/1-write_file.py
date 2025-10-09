f3ca8d5030bc6e075dc3a74e4cca52a8af3c425b
EzequielKH <laudarosa12@gmail.com>
2025-10-09 13:26:18 +0000

    wa

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
 """
-Module 1-write_file
+Module write_file
 This module provides a function that writes a string to a text file (UTF-8)
 and returns the number of characters written.
 """
 
 
-def writefile(filename="", text=""):
+def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF-8)
-    and returns the number of characters written.
-
-    Args:
-        filename (str): The name of the file to write to.
-        text (str): The string content to write into the file.
-
-    Returns:
-        int: The number of characters written to the file.
-
-    Description:
-        - Opens the file in write mode ('w'), overwriting existing content.
-        - Creates the file if it does not exist.
-        - Uses UTF-8 encoding for compatibility.
+    Writes a string to a text file (UTF-8) and returns the
+    number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as f:
         num_chars_written = f.write(text)
