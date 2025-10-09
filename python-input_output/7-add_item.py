b0b4f058575c3f9374726a1733c594296150b71d
EzequielKH <laudarosa12@gmail.com>
2025-10-09 15:31:30 +0000

    fix_pyco

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that adds all arguments to a Python list, then saves them to a JSON file.
+Script that adds all arguments to a Python list,
+then saves them to a JSON file.
 """
 
 import sys
@@ -8,6 +9,7 @@ import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 def add_items_and_save_to_json():
     """Add command-line arguments to add_item.json as a JSON list."""
@@ -18,6 +20,7 @@ def add_items_and_save_to_json():
         items.append(arg)
 
     save_to_json_file(items, filename)
+
 
 if __name__ == "__main__":
     add_items_and_save_to_json()
